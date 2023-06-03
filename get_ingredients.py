44569ceac3672992a4ca6fe1af42f93675433224
frabagocds <frabago@codigodelsur.com>
2023-06-03 17:29:20 -0300

    Get ingredients

---
+++ get_ingredients.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Read the original CSV file
+df = pd.read_csv("data_part_1.csv")
+
+# Create a new DataFrame to store the extracted ingredients
+new_df = pd.DataFrame(columns=["RecipeId", "Ingredient"])
+
+# Iterate over each row in the original DataFrame
+for index, row in df.iterrows():
+    recipe_id = row["RecipeId"]
+    ingredients = row["RecipeIngredientParts"].strip("c()").replace('"', "").split(", ")
+
+    # Create a DataFrame with the current recipe's ingredients
+    recipe_df = pd.DataFrame(
+        {"RecipeId": [recipe_id] * len(ingredients), "Ingredient": ingredients}
+    )
+
+    # Concatenate the recipe DataFrame with the new_df
+    new_df = pd.concat([new_df, recipe_df], ignore_index=True)
+
+# Save the new DataFrame to a new CSV file
+new_df.to_csv("extracted_ingredients.csv", index=False)
--- split_csv_mine.py
+++ split_csv_mine.py
@@ -1,19 +1,20 @@
-import csv 
-# Open the input file 
-with open("DATASET_DE_RECETAS/recipes.csv", "r") as input_file: 
-    # Create a CSV reader object 
-    reader = csv.reader(input_file) 
-    # Initialize a counter variable 
-    count = 0 
-    # Iterate over the rows in the input file 
-    with open("data_part_1.csv", "w") as output_file:
-        # Create a CSV writer object 
-        writer = csv.writer(output_file) 
-        for row in reader: 
-            writer.writerow(row) 
-            count += 1 
+import csv
 
-            # If the counter is equal to 1000, reset the counter 
-            if count == 100: 
-                break  
-                # count = 0 
+# Open the input file
+with open("DATASET_DE_RECETAS/recipes.csv", "r") as input_file:
+    # Create a CSV reader object
+    reader = csv.reader(input_file)
+    # Initialize a counter variable
+    count = 0
+    # Iterate over the rows in the input file
+    with open("data_part_1.csv", "w") as output_file:
+        # Create a CSV writer object
+        writer = csv.writer(output_file)
+        for row in reader:
+            writer.writerow(row)
+            count += 1
+
+            # If the counter is equal to 1000, reset the counter
+            if count == 100:
+                break
+                # count = 0
